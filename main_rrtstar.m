48a6fc66fd22711bc0f72a0d66cd70509a69abb6
minhhuster0508-cmyk <minhhuster0508@gmail.com>
2025-10-06 21:52:53 +0700

    Update main_rrtstar.m

--- main_rrtstar.m
+++ main_rrtstar.m
@@ -7,11 +7,12 @@ addpath(genpath('core')); addpath('config'); addpath('utils');
 
 % Load map & parameters
 cfg = map_config();
-
+[cfg.start, cfg.goal] = pick_start_goal(cfg.map, cfg.goal_radius);
 
 % Run RRT*
 [path, tree] = rrtstar(cfg);
 
 
 % Visualize
+
 visualize(cfg.map, cfg.start, cfg.goal, path, tree);
